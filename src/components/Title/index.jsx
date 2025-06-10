74139490921670b811abde8ce7e3a6dfad10fa59
Anett Seeker <noeddl@users.noreply.github.com>
2025-06-10 08:55:32 +0200

    💬 Update Title component

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -3,8 +3,8 @@ import { Box, Image, Text } from '@chakra-ui/react';
 function Title() {
     return (
         <Box alignItems='center'>
-            <Image src='https://neal.fun/spend/billgates.jpg' m={'auto'} borderRadius='full' />
-            <Text fontSize={40} mt={3} m={'auto'}  fontWeight={700}>Spend Bill Gates' Money</Text>
+            <Image src='https://media.frag-den-staat.de/files/media/main/49/dd/49dd4b08-fab7-41c6-9b7a-a1531a5a6417/spahn.png' width={250} height={250} m={'auto'} borderRadius='full' />
+            <Text fontSize={40} mt={3} m={'auto'}  fontWeight={700}>Verteile Spahns Milliarden</Text>
         </Box>
     );
 }
