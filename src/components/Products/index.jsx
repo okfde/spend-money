a158f3a3642b38839d932241847318344ccfd227
Anett Seeker <noeddl@users.noreply.github.com>
2025-06-11 09:34:16 +0200

    💄 Reduce maximum width

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -8,7 +8,7 @@ function Products() {
   const currentMoney = useSelector((state) => state.product.currentMoney);
 
   return (
-    <Container fluid style={{ maxWidth: "1400px" }}>
+    <Container fluid style={{ maxWidth: "1300px" }}>
       <div
         className="mt-3 mb-3 bg-primary text-white sticky-top py-2"
         style={{
