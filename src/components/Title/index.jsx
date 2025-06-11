4a8e80fd1a485b87ab2b74a95b5d207c3cf0893b
Anett Seeker <noeddl@users.noreply.github.com>
2025-06-11 12:50:06 +0200

    💬 Add explanation text

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -12,6 +12,11 @@ function Title() {
         <h1 className="fs-1 mt-3 fw-bold mx-auto">
           Verteile Spahns 3,1 Milliarden
         </h1>
+        <div className="fs-5 text-center mt-2 mb-4">
+          3,1 Milliarden Euro – <a href="https://fragdenstaat.de/artikel/exklusiv/2025/06/spahn-brh/" target="_blank" rel="noopener noreferrer">so viel hat Jens Spahn laut Bundesrechnungshof planlos verpulvert</a>.
+          <br />
+          Was würdest du mit dem Geld machen? Verteil’s besser!
+        </div>
       </Container>
     );
 }
