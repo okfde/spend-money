51f2799a886f18db1add9f2d40aa51b08adc440d
Anett Seeker <noeddl@users.noreply.github.com>
2025-06-11 14:20:08 +0200

    💄 Extend maximum width

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -8,7 +8,7 @@ function Products() {
   const currentMoney = useSelector((state) => state.product.currentMoney);
 
   return (
-    <Container fluid style={{ maxWidth: "1300px" }}>
+    <Container fluid style={{ maxWidth: "1320px" }}>
       <div
         className="mt-3 mb-3 bg-primary text-white sticky-top py-2"
         style={{
